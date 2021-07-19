1045b27cfa56bf678b51076172764af96839883b
lexypen <alexia.penney@hotmail.com>
2021-07-19 15:06:05 -0400

    Commited TTT

---
+++ app/auth/events.js
@@ -0,0 +1,44 @@
+'use strict'
+const getFormFields = require('./../../lib/get-form-fields')
+const api = require('./api')
+const ui = require('./ui')
+const onSignUp = function (event) {
+  event.preventDefault()
+  // console.log('Hello')
+  // Get information from event and form
+  const form = event.target
+  // console.log(form)
+  const data = getFormFields(form)
+  // console.log(data)
+  // Make an API call using AJAX
+  api.signUp(data)
+  // Handle successful API calls with .then
+    .then(ui.onSignUpSuccess)
+  // Handle failed API calls with .catch
+    .catch(ui.onSignUpFailure)
+}
+const onSignIn = function (event) {
+  event.preventDefault()
+  // console.log('Hello')
+  // Get information from event and form
+  const form = event.target
+  // console.log(form)
+  const data = getFormFields(form)
+  // console.log(data)
+  // Make an API call using AJAX
+  api.signIn(data)
+  // Handle successful API calls with .then
+    .then(ui.onSignInSuccess)
+  // Handle failed API calls with .catch
+    .catch(ui.onSignInFailure)
+}
+const onSignOut = function () {
+  api.signOut()
+    .then(ui.onSignOutSuccess)
+    .catch(ui.onSignOutFailure)
+}
+module.exports = {
+  onSignUp,
+  onSignIn,
+  onSignOut
+}
---
+++ app/auth/ui.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const store = require('./../store')
+
+const onSignUpSuccess = (response) => {
+// console.log('Hooray!')
+  $('#message').text(`Thank you for signing up ${response.user.email}`)
+  console.log(response)
+  $('#sign-up').trigger('reset')
+}
+
+const onSignUpFailure = () => {
+// console.log('Failure')
+  $('#message').text('Sign up failure')
+  $('#sign-up').trigger('reset')
+}
+
+const onSignInSuccess = (response) => {
+  // console.log('Hooray!')
+  $('#message').text(`Thank you for signing in ${response.user.email}.`)
+  store.token = response.user.token
+  $('#sign-in').trigger('reset')
+  $('#sign-in').hide()
+  $('#sign-up').hide()
+  $('#sign-out').show()
+}
+
+const onSignInFailure = () => {
+  // console.log('Failure')
+  $('#message').text('Sign in failure')
+  $('#sign-in').trigger('reset')
+}
+
+const onSignOutSuccess = () => {
+  // console.log('Success')
+  $('#message').text('Successfully signed out')
+  $('#sign-in').show()
+  $('#sign-up').show()
+  $('#sign-out').hide()
+}
+
+const onSignOutFailure = () => {
+  // console.log('Failure')
+  $('#message').text('Failed to sign out')
+}
+
+module.exports = {
+  onSignUpSuccess,
+  onSignUpFailure,
+  onSignInSuccess,
+  onSignInFailure,
+  onSignOutSuccess,
+  onSignOutFailure
+}
