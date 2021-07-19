41077ab9ddbb2bd8b78ff4dfc549a3da07bc7e72
lexypen <alexia.penney@hotmail.com>
2021-07-19 15:23:11 -0400

    Using API URL

--- app/auth/api.js
+++ app/auth/api.js
@@ -1,11 +1,11 @@
 'use strict'
 
-const store = require('./../store')
-
+const store = require('../store')
+const config = require('../config')
 const signUp = function (data) {
   // console.log(data)
   return $.ajax({
-    url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-up',
+    url: config.apiUrl + '/sign-up',
     method: 'POST',
     data: data
   })
@@ -14,14 +14,14 @@ const signUp = function (data) {
 const signIn = function (data) {
   // console.log(data)
   return $.ajax({
-    url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-in',
+    url: config.apiUrl + '/sign-in',
     method: 'POST',
     data: data
   })
 }
 const signOut = function () {
   return $.ajax({
-    url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-out',
+    url: config.apiUrl + '/sign-out',
     method: 'DELETE',
     headers: {
       Authorization: 'Bearer ' + store.token
